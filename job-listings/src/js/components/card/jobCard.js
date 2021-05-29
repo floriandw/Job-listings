499e327635ddbaaefa2b758a48a96df42a906e5d
Florian De Wulf <florian.dewulf@student.odisee.be>
2021-05-29 22:32:13 +0200

    added avatar for job

--- job-listings/src/js/components/card/jobCard.js
+++ job-listings/src/js/components/card/jobCard.js
@@ -1,9 +1,10 @@
 import React,{ useState, useEffect } from 'react';
 
 import { useStyles } from '../../styles/jobCard';
-import { Card, Divider, CardContent, Typography  } from '@material-ui/core';
+import { Card, Avatar, Divider, CardContent, Typography  } from '@material-ui/core';
 import Tag from "../tags/tag";
 import InfoTags from "../tags/infoTags";
+import myhome from "./images/myhome.svg"
 
 const JobCard = (props) => {
     const job = props.job
@@ -11,15 +12,18 @@ const JobCard = (props) => {
     const classes = useStyles();
     console.log(props);
     console.log(props.role);
-    const [info, setInfo] = useState([job.role, job.level, ...job.tools, ...job.languages]);
+    const [info] = useState([job.role, job.level, ...job.tools, ...job.languages]);
 
 
-    console.log(info);
-    console.log(job.featured);
+    console.log(myhome);
+    console.log(typeof job.logo);
 
     return (
+        <>
+       <Avatar style={{position:"absolute", zIndex:"1", marginTop: "-18px",left: "46px"}} alt="Remy Sharp" src={job.logo} />
         <Card className={`${classes.root} ${job.featured ? classes.featured : ""}`} >
-            <CardContent style={{whiteSpace:'pre-wrap'}}>
+            {console.log(job.logo)}
+            <CardContent style={{whiteSpace:'pre-wrap', marginTop: "15px"}}>
                 <Typography style={{float:"left",marginRight: "10px", marginBottom:"12px", fontFamily:"spartan",fontWeight: 700, color: "#5da5a4"}}>
                    {job.company}
                 </Typography>
@@ -31,7 +35,7 @@ const JobCard = (props) => {
                    {job.position}
                 </Typography>
                 <Typography style={{marginBottom: "15px", color:"#7B8E8E"}}>
-                    {job.postedAt} • {job.contract} • {job.location}
+                    {job.postedAt}  •  {job.contract}  •  {job.location}
                 </Typography>
                 <Divider  />
                 <Typography style={{marginTop: "15px", display: "block"}} component={'span'} >
@@ -39,6 +43,7 @@ const JobCard = (props) => {
                 </Typography>
             </CardContent>
         </Card>
+        </>
     )
 }
 
