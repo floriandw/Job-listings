1af1a5eedf9558d90f6a62763fdd403573ddd114
Florian De Wulf <florian.dewulf@student.odisee.be>
2021-05-31 13:06:37 +0200

    setup redux store for filters

--- job-listings/src/js/components/tags/infoTag.js
+++ job-listings/src/js/components/tags/infoTag.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import { useDispatch } from 'react-redux'
 
-import { Card } from "@material-ui/core";
+import { useSelector } from "react-redux";
+import { Card, } from "@material-ui/core";
+import { useStyles } from '../../styles/infoTagStyle';
+import { addShopItem } from "../../store/actions/filterActions"
+
 
 const InfoTag = (props) => {
-    return (
-        <Card elevation={0} style={{display:"inline-block", width: "auto",backgroundColor: "#EEF6F6",paddingLeft:"10px",paddingRight:"10px",paddingTop:"7px", paddingBottom:"7px",color: "#5da5a4", margin: "5px",marginRight:"15px", fontWeight:"700",verticalAlign: "middle", fontSize: "15px"}}>
+    const dispatch = useDispatch()
+    const classes = useStyles();
+   
+    const addTagFilter= (props) => { 
+        const name = props.item
+        dispatch(addShopItem(name))
+    }
+
+    return (  
+        <Card onClick={()=> {addTagFilter(props)}} elevation={0} className={classes.root} >
             {props.item}
         </Card>
     );
